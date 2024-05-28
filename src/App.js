2c600ad889cf9d7b4a1ca62f09ba25f8c8c50d88
realityzero <nishantsikri@spit.ac.in>
2024-05-28 15:12:44 +0530

    useReducer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import SideEffectButton from './components/SideEffectComponent';
 import UseMemoComponent from './components/UseMemoComponent';
 import { UseRefComponent } from './components/UseRefComponent';
 import { ContextComponent } from './components/ContextComponent';
+import ReducerComponent from './components/ReducerComponent';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
       <UseRefComponent></UseRefComponent>
       <br/>
       <ContextComponent/>
+      <ReducerComponent/>
     </div>
   );
 }
